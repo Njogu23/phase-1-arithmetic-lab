a5ec698cb21d1638173dba879833606363ace381
George Njogu <sbeveme@gmail.com>
2022-08-18 12:30:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-let num1 = 31;
-let num2 = 2;
+let num1 = 2;
+let num2 = 31;
 let multiply = num1 * num2;
 
-let random = Math.floor(Math.random()* 10) + 1;
+let random = Math.ceil(Math.random());
 
 let num3 = 24;
 let num4 = 5;
-let mod = parseInt(num3/num4, 10);
+let mod = num3 % num4;
 
-let max = Math.max(1,2,20,13);
-
-
-
-
-
-
-
-
+let max = Math.max(1,2,3,20,6);
